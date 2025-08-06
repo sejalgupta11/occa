fe2c7b63fbb61cd7653a0b74f1402db852d66df2
Sejal Gupta <sejalg8634@gmail.com>
2025-08-06 14:21:08 -0400

    mangled runtime04 for debugging

--- examples_new/C++/runtime04-streams.cpp
+++ examples_new/C++/runtime04-streams.cpp
@@ -2,8 +2,9 @@
 #include <occa.hpp>
 
 int main(int argc, const char **argv) {
-    int chunks = 6; 
-    int chunkSize = 64;
+    int chunks = 1; 
+    // int chunkSize = 64;
+    int chunkSize = 1;
     int entries = chunks * chunkSize;
 
     std::cout << "Number of entries: " << entries << std::endl; 
@@ -16,22 +17,24 @@ int main(int argc, const char **argv) {
     occa::device device({{"mode", "Serial"}}); 
 
     occa::stream streamCopy = occa::getStream();
-    occa::stream streamKernel = occa::createStream();
+    // occa::stream streamKernel = occa::createStream();
 
     occa::memory o_chunk[2];
     for (int i = 0; i < 2; ++i)
         o_chunk[i] = occa::malloc<float>(chunkSize);
 
+    float foo[] = {3.0f};
     occa::memory o_results = occa::malloc<float>(chunks);
+    o_results.copyFrom(foo,1,0);
 
     occa::kernel sumReduce = occa::buildKernel("reduction.okl", "reduction");
-    occa::json async_copy({{"async", true}});
+    // occa::json async_copy({{"async", true}});
 
     
 
-    occa::setStream(streamCopy);
+    // occa::setStream(streamCopy);
     
-    o_chunk[0].copyFrom(a, chunkSize, 0, async_copy);
+    o_chunk[0].copyFrom(a, chunkSize, 0);
     
     float temp = 4; 
 
@@ -53,39 +56,49 @@ int main(int argc, const char **argv) {
 
         // Copy next chunk for future loop iteration
         if (i < chunks - 1) {
-            occa::setStream(streamCopy);
-            o_chunk[next].copyFrom(a + (i + 1) * chunkSize, chunkSize, 0, async_copy);
+            // occa::setStream(streamCopy);
+            // o_chunk[next].copyFrom(a + (i + 1) * chunkSize, chunkSize, 0, async_copy);
+            o_chunk[next].copyFrom(a + (i + 1) * chunkSize, chunkSize, 0);
+            // //print debug
+        // for (int j=0; j<chunkSize; j++){
+        //     (o_chunk[next] + j).copyTo(&temp);
+        //     std::cout << temp << ' '; 
+        // }
+        // std::cout << std::endl; 
         }
 
-// //print debug
-//         for (int j=0; j<chunkSize; j++){
-//             (o_chunk[next] + j).copyTo(&temp);
-//             std::cout << temp << ' '; 
-//         }
-//         std::cout << std::endl; 
+
 
         // Make sure copy for current chunk is finished before kernel laucnh usinh streamTags
-        occa::setStream(streamCopy);
+        // occa::setStream(streamCopy);
         occa::streamTag copyDone = occa::tagStream(); // todo explain streamtag more
-        occa::waitFor(copyDone);
+        
 
        
 
-        // Launch reduction on the previously copied chunk
-        occa::setStream(streamKernel);
-        sumReduce.setRunDims(chunkSize, 1);
-        sumReduce(chunkSize, o_chunk[cur], o_results + i);
+        // Switch stream back to kernel stream
+        // occa::setStream(streamKernel);
+        occa::waitFor(copyDone);
+
+        // Set run dims and launch 
+        // sumReduce.setRunDims(chunkSize, 1);
+        occa::memory& chunk_0 = o_chunk[0];
+        // sumReduce(chunkSize, o_chunk[cur], o_results);
+        sumReduce(chunkSize, chunk_0, o_results);
 
     }
 
     // Wait for kernel stream to finish all work
-    occa::setStream(streamKernel);
-    streamKernel.finish();
+    // occa::setStream(streamKernel);
+    // streamKernel.finish();
 
     // Copy results to host
     float *hostResults = new float[chunks];
-    o_results.copyTo(hostResults);
+    hostResults[0] = 1.0f; 
+    // o_results.copyFrom(hostResults,1,0);
+    o_results.copyTo(hostResults,1,0);
 
+    device.finish();
     // do final reduction 
     float totalSum = 0;
     for (int i = 0; i < chunks; ++i) {
